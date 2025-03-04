36afb7a1ceb552d5d5c30ac16e04b84174e1cb6d
Alvaro Henrique <alvaroindaial15@gmail.com>
2025-03-04 16:53:33 -0300

    feat: cria usuarios no banco

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,12 +1,32 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { hashSync } from 'bcrypt';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    
-    return 'This action adds a new user';
+  constructor(private prisma: PrismaService) { }
+
+  async create(createUserDto: CreateUserDto) {
+
+    const saltOrRounds = 10;
+    const hash = hashSync(createUserDto.password, saltOrRounds);
+
+    const user = await this.prisma.user.create({
+      data: {
+        name: createUserDto.name,
+        email: createUserDto.email,
+        password: hash
+      },
+      select: {
+        id: true,
+        name: true,
+        email: true
+      }
+    })
+
+    return user;
   }
 
   findAll() {
