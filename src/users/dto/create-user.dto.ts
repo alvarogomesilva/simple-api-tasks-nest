3a341eb8fc8d224d7122f1058f303a4ea95b7d7d
Alvaro Henrique <alvaroindaial15@gmail.com>
2025-03-05 10:59:49 -0300

    feat: valida criacao de usuarios

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,17 @@
+import {  IsNotEmpty, IsString, MinLength } from 'class-validator';
+
 export class CreateUserDto {
+
+    @IsString({ message: 'Nome precisa ser um texto' })
+    @IsNotEmpty({ message: 'Nome não pode ser vazio' })
     name: string;
+
+    @IsString({ message: 'Email precisa ser um texto' })
+    @IsNotEmpty({ message: 'Email não pode ser vazio' })
     email: string;
+
+    @IsString({ message: 'Senha precisa ser um texto' })
+    @IsNotEmpty({ message: 'Senha não pode ser vazio' })
+    @MinLength(6, { message: 'A senha precisa ter pelo menos 6 caracteres' })
     password: string;
 }
