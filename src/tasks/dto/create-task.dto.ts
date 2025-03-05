1d39636c4f52482d3dc11c5484487ed19d40e6d3
Alvaro Henrique <alvaroindaial15@gmail.com>
2025-03-05 11:06:46 -0300

    feat: valida criacao das tarefas

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,5 +1,15 @@
+import { IsNotEmpty, IsString } from "class-validator";
+
 export class CreateTaskDto {
+
+    @IsString({ message: 'Nome precisa ser um texto' })
+    @IsNotEmpty({ message: 'Nome não pode ser vazio!' })
     name: string;
+
+    @IsString({ message: 'Descrição precisa ser um texto' })
+    @IsNotEmpty({ message: 'Descrição não pode ser vazio' })
     description: string;
+
+    @IsNotEmpty({ message: 'Usuário inválido' })
     userId: number;
 }
