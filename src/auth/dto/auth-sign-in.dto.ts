a35239d7c935ea3cb15cc3f3e3c1f488e847a105
Alvaro Henrique <alvaroindaial15@gmail.com>
2025-03-06 08:17:24 -0300

    feat: valida DTO de autenticacao

--- src/auth/dto/auth-sign-in.dto.ts
+++ src/auth/dto/auth-sign-in.dto.ts
@@ -1,5 +1,13 @@
+import { IsEmail, IsNotEmpty, IsString, MinLength } from "class-validator";
 
 export class AuthSignInDto {
+
+    @IsString({ message: 'Email precisa ser um texto' })
+    @IsNotEmpty({ message: 'Email não pode ser vazio' })
     email: string;
+
+    @IsString({ message: 'Senha precisa ser um texto' })
+    @IsNotEmpty({ message: 'Senha não pode ser vazio' })
+    @MinLength(6, { message: 'A senha precisa ter pelo menos 6 caracteres' })
     password: string;
 }
